dbf4b4507263b96f42a6815d60a1390f2a45ec85
Vlad Logan <vladlogan.log@gmail.com>
2024-05-03 19:32:56 +0300

    Version before deleting components/game

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ export default function HomePage() {
   return (
     <div className="bg-slate-50 min-h-screen">
       <Header />
+      <main className="pt-6 mx-auto max-w-[616px]"></main>
     </div>
   );
   // return <Game />;
