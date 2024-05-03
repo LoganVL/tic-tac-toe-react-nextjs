d101bb299f7935c70a56e75b2684dc88a3a0ceb1
Vlad Logan <vladlogan.log@gmail.com>
2024-05-04 00:35:10 +0300

    Header and Profile added and  separated, Game folder remade with new architecture, GameInfo added

--- components/game/game-info.jsx
+++ components/game/game-info.jsx
@@ -1,21 +1,36 @@
-import { GameSymbol } from "./game-symbol";
+import clsx from "clsx";
+import { Profile } from "../profile";
+import { CrossIcon } from "./icons/cross-icon";
 
-export function GameInfo({ gameState, winnerSymbol, currentStep }) {
-  if (gameState === "Ничья") {
-    return <div className="mb-2.5">{gameState}</div>;
-  }
-  if (gameState === "Ход:") {
-    return (
-      <div className="mb-2.5">
-        {gameState} {<GameSymbol symbol={currentStep} />}
+export function GameInfo({ className }) {
+  return (
+    <div
+      className={clsx(
+        className,
+        "bg-white rounded-2xl shadow-md px-8 py-4 flex justify-between",
+      )}
+    >
+      <div className="flex gap-3 items-center">
+        <div className="relative">
+          <Profile className="w-44" />
+          <div className="flex items-center justify-center w-5 h-5 rounded-full bg-white shadow absolute -left-1 -top-1">
+            <CrossIcon />
+          </div>
+        </div>
+        <div className="h-6 w-px bg-slate-200"></div>
+        <div className="text-slate-500 text-lg font-semibold">01:48</div>
       </div>
-    );
-  }
-  if (gameState === "Победитель:") {
-    return (
-      <div className="mb-2.5">
-        {gameState} {<GameSymbol symbol={winnerSymbol} />}
+
+      <div className="flex gap-3 items-center">
+        <div className="text-orange-500 text-lg font-semibold">00:08</div>
+        <div className="h-6 w-px bg-slate-200"></div>
+        <div className="relative">
+          <Profile className="w-44" />
+          <div className="flex items-center justify-center w-5 h-5 rounded-full bg-white shadow absolute -left-1 -top-1">
+            <CrossIcon />
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
---
+++ components/profile/profile.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import avatarSrc from "./avatar.png";
+import { clsx } from "clsx";
+
+export function Profile({ className }) {
+  return (
+    <div
+      className={clsx(
+        "flex items-center gap-2 text-start text-teal-600",
+        className,
+      )}
+    >
+      <Image src={avatarSrc} width={48} height={48} alt="avatar" unoptimized />
+      <div>
+        <div className=" text-lg leading-tight hover:text-teal-500 transition-colors">
+          Placeholder
+        </div>
+        <div className="text-slate-400 text-xs leading-tight">
+          Рейтинг: 1230
+        </div>
+      </div>
+    </div>
+  );
+}
